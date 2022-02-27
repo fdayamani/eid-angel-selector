66a7601fdb9f9888cf7580502876ceb180f02a56
Fatema Damani <fatema.damani@gmail.com>
2022-02-27 22:41:15 +0000

    Write output to a file so there is a record

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,13 @@
-import data.DataHandler;
 import domain.Child;
 import domain.Selector;
 import output.OutputHandler;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.Map;
+import java.util.Random;
 
 import static data.DataHandler.transformData;
 
@@ -15,7 +17,10 @@ public class App {
         String responsesSheetId = "1CbVUmNPBoEjvDI1fWA2AsHnVO0NZk1QvFpzApBqlM6w";
         Selector selector = new Selector(transformData(responsesSheetId));
         Map<Child, Child> allocations = selector.assignAngels();
-        System.out.println(OutputHandler.outputMessages(allocations));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(String.format("allocations-%d.txt", System.currentTimeMillis())));
+        writer.write(OutputHandler.outputMessages(allocations).toString());
+
+        writer.close();
     }
 
 }
